f0bd02c22773221feeb5a2b39710e44d5d92b31f
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-03-28 00:18:11 +0600

    add to cart fully functional without any zero multiplication related bugs

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,23 +2,29 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({cart}) => {
-    const totalPrice = addToCart(cart, cart.price);
-    const totalShipping = addToCart(cart, cart.shipping);
-    const tax = addToCart(cart, cart.price)*10/100;
+    let totalPrice = 0;
+    let totalShipping = 0;
+    let quantity = 0;
+
+    for(const product of cart) {
+        // product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
+    }
+
+    const tax = totalPrice*10/100;
+
     return (
         <aside className='cart'>
             <h3>Order Summary</h3>
-            <p>Selected Items: {cart.length}</p>
-            <p>Total Price: ${totalPrice.toFixed(2)}</p>
-            <p>Total Shipping: ${totalShipping.toFixed(2)}</p>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping: ${totalShipping}</p>
             <p>Tax: ${tax}</p>
-            <h6>Grand Total: ${(totalPrice + totalShipping + tax).toFixed(2)} </h6>
+            <h6>Grand Total: ${(totalPrice + totalShipping + tax).toFixed(2)}</h6>
         </aside>
     );
-};
-
-function addToCart(cart, price) {
-    return cart.reduce((prev, curr) => prev + curr.price, 0)
 }
 
 export default Cart;
